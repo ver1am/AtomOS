78b1ef1eedfcca0be06015a926ce1cba740ed541
Vovchik <vovchokvova10@gmail.com>
2026-09-12 15:33:17 +0300

    Add trim function and improve symbol handling
    Added a trim function to remove newline and tab characters from text. Modified asm_setsymbol and asm_compile functions for better symbol handling.

--- atasm.c
+++ atasm.c
@@ -9,6 +9,17 @@ uint8_t msize(uint8_t* m) {
 	return i;
 }
 
+/*
+	\n \r \t
+*/
+void trim(char* text) {
+	for (uint8_t i = 0; text[i] != '\0'; i++) {
+		if (text[i] == '\n' || text[i] == '\r' || text[i] == '\t') {
+			
+		}
+	}
+}
+
 void skip_chars(char* txt, uint8_t* i) {
 	while (
 		txt[*i] == ' '  ||
@@ -74,11 +85,14 @@ bool stringscanf(char* text, char* format,...) {
 				i++;
 			} else if (format[i+1] == 's') {
 				char* rtxt = va_arg(args,char*);
-				rtxt = &text[i];
-
-				arg_len(text,&i,format[i+2]);
-				text[i+1] = '\0';
-				i++;
+				uint8_t j = 0;
+				while (text[i] != '\0' && text[i] != format[i+2]) {
+					if (text[i] != '\n' && text[i] != '\r') {
+						rtxt[j++] = text[i];
+					}
+					i++;
+				}
+				rtxt[j] = '\0';
 			}
 		} else if (text[i] != format[i]) {
 			// Probably missed i-s
@@ -103,6 +117,46 @@ CMD_T x86[] = {
 	{.cmdf = "syscall", .byte = {0x0F,0x05,0},  .arg_t = ARG_NONE}
 };
 
+void asm_setsymbol(char* asmcode,SYMBOL_T symbols[],int8_t lastsymbol,uint32_t* pc) {
+	char temp[20] = {0};
+	uint8_t ptr = 0;
+	printf("setsymbol\n\r");
+	for (uint8_t i = 0; i < sizeof(x86) / sizeof(x86[0]); i++) {
+		if (stringscanf(asmcode,x86[i].cmdf,temp)) {
+
+			for (uint8_t j = 0;x86[i].byte[j] != 0;j++) ptr++;
+
+			if (x86[i].arg_t == ARG_INT8) {
+				ptr++;
+			}
+			else if (x86[i].arg_t == ARG_INT32) {
+				ptr++;
+				ptr++;
+				ptr++;
+				ptr++;
+			} else if (x86[i].arg_t == ARG_TAG) {
+				ptr++;
+			}
+
+			*pc += ptr;
+		} else {
+			uint8_t c = 0;
+			arg_len(asmcode,&c,':');
+			if (asmcode[c+1] == ':') {
+				asmcode[++c] = '\0';
+				for (uint8_t i = 0;i < lastsymbol;i++) {
+					if (strcmp(symbols[i].text,asmcode) == 0) {
+						printf("Finded :%s\n\r",asmcode);
+						symbols[i].address = *pc;
+						break;
+					}
+				}
+			}
+			printf("text: %s,lastsymbol: %d\n\r",asmcode,lastsymbol);
+		}
+	}
+}
+
 uint8_t asm_tobyte(char* asmcode, uint8_t* bytes, SYMBOL_T symbols[], int8_t lastsymbol,uint32_t* pc) {
 	char temp[20] = {0};
 	uint8_t ptr = 0;
@@ -122,10 +176,10 @@ uint8_t asm_tobyte(char* asmcode, uint8_t* bytes, SYMBOL_T symbols[], int8_t las
 				bytes[ptr++] = (uint8_t)((temp[2] >> 16) & 0xFF);
 				bytes[ptr++] = (uint8_t)((temp[3] >> 24) & 0xFF);
 			} else if (x86[i].arg_t == ARG_TAG) {
-				printf("AAA: %s\n\r",temp);
 				for (;lastsymbol >= 0;lastsymbol--) {
 					if (strcmp(symbols[lastsymbol].text,temp) == 0) {
 						bytes[ptr++] = symbols[lastsymbol].address - *pc;
+						printf("adr %u\n\r",symbols[lastsymbol].address - *pc);
 					}
 				}
 			}
@@ -133,6 +187,7 @@ uint8_t asm_tobyte(char* asmcode, uint8_t* bytes, SYMBOL_T symbols[], int8_t las
 			*pc += ptr;
 		}
 	}
+	return ptr;
 }
 
 void asm_psymbol(char* buffer,SYMBOL_T symbols[],uint8_t* symbol) {
@@ -141,8 +196,12 @@ void asm_psymbol(char* buffer,SYMBOL_T symbols[],uint8_t* symbol) {
 	char* ptr = &buffer[i];
 	arg_len(buffer,&i,':');
 	if (buffer[i+1] == ':') {
+		printf("A\n\r");
 		buffer[i+1] = '\0';
-		strcpy(symbols[(*symbol)++].text,ptr);
+		uint8_t idx = *symbol;
+		strcpy(symbols[idx].text,ptr);
+		*symbol = idx + 1;
+		printf("TEST\n\r");
 	}
 }
 
@@ -150,24 +209,38 @@ void asm_compile(FILE* file,FILE* bin) {
 	char buffer[128] = {0};
 
 	SYMBOL_T symbols[50] = {0}; // For "label:"
-	uint8_t lastsymbol = 0;
+	int8_t lastsymbol = 0;
 
 	uint32_t pc = 0;
 
 	// Symbol parser
 	while (fgets(buffer,sizeof(buffer),file) != NULL) {
-		asm_psymbol(&buffer[0],symbols,&lastsymbol);
+		asm_psymbol(buffer,symbols,&lastsymbol);
 	}
 
+	printf("S\n\r");
+
+	clearerr(file);
 	fseek(file,0,SEEK_SET);
 
+	printf("1 : %u\n\r",lastsymbol);
+	uint8_t i = 0;
+	while (fgets(buffer,sizeof(buffer),file) != NULL) {
+		printf("Iteration: %u\n\r",i++);
+		asm_setsymbol(buffer,symbols,lastsymbol,&pc);
+	}
+	printf("2\n\r");
+
+	clearerr(file);
+	fseek(file,0,SEEK_SET);
+
+	pc = 0;
 	uint8_t error = 1; // 0 if error
 
 	while (fgets(buffer,sizeof(buffer),file) != NULL) {
 		char byte[20] = {0};
 		uint8_t b_c = asm_tobyte(buffer,byte,symbols,lastsymbol,&pc);
 		error = (b_c != 0);
-		pc += b_c;
 		fwrite(byte,b_c,1,bin);
 	}
 	if (!error) {
