d3532bd6bea947813019931034c2bf721769394b
Vovchik <vovchokvova10@gmail.com>
2026-09-16 13:13:59 +0300

    Refactor asm code handling and improve symbol management
    Refactor assembly code parser and update data structures.

--- atasm.c
+++ atasm.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <string.h>
 #include <stdint.h>
-#include <stdarg.h>
+
+#include "port.h"
 
 uint8_t msize(uint8_t* m) {
 	uint16_t i = 0;
@@ -68,27 +68,6 @@ void arg_len(char* txt, uint8_t* i, uint8_t byte) {
 	) (*i)++;
 }
 
-typedef struct {
-	char text[20];
-	uint32_t address;
-} SYMBOL_T;
-
-typedef enum {
-	ARG_NONE,
-	ARG_REGB,
-	ARG_REGD,
-	ARG_INT32,
-	ARG_INT8,
-	ARG_TAG,
-} ARGS_T;
-
-typedef struct {
-	char* cmdf;
-	uint8_t byte[3];
-	ARGS_T arg0_t;
-	ARGS_T arg1_t;
-} CMD_T;
-
 int to_num(char* txt, uint8_t size) {
 	int num = 0;
 	bool neg = (txt[0] == '-');
@@ -116,210 +95,42 @@ int16_t stepen(int16_t num,uint16_t mon) {
 	return rnum;
 }
 
-/*
-	%d - to number
-	%s - to tag
-*/
-bool stringscanf(char* text, char* format,...) {
-	va_list args;
-	va_start(args,format);
-
-	trim_end(text);
-	for (uint8_t i = 0;text[i] != '\0';i++) {
-		if (format[i] == '%') {
-			if (format[i+1] == 'd') {
-				int* num = va_arg(args, int*);
-				uint8_t last = i;
-				*num = 0;
-				if (text[i] == '0' && text[i+1] == 'x') {
-					i += 2;
-					last = i;
-					arg_len(text,&i,format[i+2]);
-					uint8_t idx = 0;
-					for (int8_t c = i;last <= c;c--) {
-						*num += hexindex(text[c]) * stepen(16,idx++);
-					}
-				} else {
-					arg_len(&text[i],&i,format[i+2]);
-					*num = to_num(&text[last],i-last);
-				}
-				i++;
-			} else if (format[i+1] == 's') {
-				char* rtxt = va_arg(args,char*);
-				uint8_t j = 0;
-				while (text[i] != '\0' && text[i] != format[i+2]) {
-					if (text[i] != '\n' && text[i] != '\r') {
-						rtxt[j++] = text[i];
-					}
-					i++;
-				}
-				rtxt[j] = '\0';
-			} else if (format[i+1] == 'r') {
-				char txt[5] = {0};
-
-				uint8_t* rbyte = NULL;
-				uint8_t byte = 0;
-
-				uint8_t first = i;
-				arg_len(text,&i,format[i+2]);
-
-				uint8_t len = i-first;
-
-				for (uint8_t c = 0;c < (i+1)-first;c++) {
-					txt[c] = text[first+c];
-				}
-
-				txt[(i+1)-first] = '\0';
-
-				// 8 bits 0-7
-				if (len == 1 || len == 2) {// If AL CL DL BL AH CH DH BH
-					rbyte = va_arg(args,uint8_t*);
-					byte = *rbyte;
-					if (strcmp(txt,"al") == 0        || strcmp(txt,"eax") == 0) {
-						byte += 0;
-					} else if (strcmp(txt,"cl") == 0 || strcmp(txt,"ecx") == 0) {
-						byte += 0x01;
-					} else if (strcmp(txt,"dl") == 0 || strcmp(txt,"edx") == 0) {
-						byte += 0x02;
-					} else if (strcmp(txt,"bl") == 0 || strcmp(txt,"ebx") == 0) {
-						byte += 0x03;
-					} else if (strcmp(txt,"ah") == 0 || strcmp(txt,"esp") == 0) {
-						byte += 0x04;
-					} else if (strcmp(txt,"ch") == 0 || strcmp(txt,"ebp") == 0) {
-						byte += 0x05;
-					} else if (strcmp(txt,"dh") == 0 || strcmp(txt,"esi") == 0) {
-						byte += 0x06;
-					} else if (strcmp(txt,"bh") == 0 || strcmp(txt,"edi") == 0) {
-						byte += 0x07;
-					}
-				} else {
-					i = first;
-					continue;
-				}
-				*rbyte = byte;
-				i += 2;
-			}
-		} else if (text[i] != format[i]) {
-			va_end(args);
-			return false;
-		}
-	}
-
-	va_end(args);
-	return true;
-}
-
-/*
-	%r - Registers REG
-	%d - Number ex. F6 = -10 00 00 00 00 INT32
-	%s - Tag TAG
-*/
-CMD_T x86[] = {
-	// mov
-	{.cmdf = "mov bl, al",.byte = {0x88,0xC3,0},   .arg0_t = ARG_NONE,.arg1_t = ARG_NONE},
-	{.cmdf = "mov ebx, eax",.byte = {0x89,0xC3,0}, .arg0_t = ARG_NONE,.arg1_t = ARG_NONE},
-	{.cmdf = "mov %r, %d",.byte = {0,0,0},         .arg0_t = ARG_REGB,.arg1_t = ARG_INT8},
-
-	// call
-	{.cmdf = "call %r",.byte = {0xFF,0,0},         .arg0_t = ARG_REGD,.arg1_t = ARG_NONE},
-	{.cmdf = "call %d",.byte = {0xE8,0,0},         .arg0_t = ARG_INT32,.arg1_t = ARG_NONE},
-
-	{.cmdf = "inc eax", .byte = {0x40,0,0},        .arg0_t = ARG_NONE,.arg1_t = ARG_NONE},
-	{.cmdf = "cmp eax, %d", .byte = {0x3D,0,0},    .arg0_t = ARG_INT32,.arg1_t = ARG_NONE},
-	{.cmdf = "jl %s", .byte = {0x7C,0,0},          .arg0_t = ARG_TAG,.arg1_t = ARG_NONE},
-	{.cmdf = "syscall", .byte = {0x0F,0x05,0},     .arg0_t = ARG_NONE,.arg1_t = ARG_NONE},
-	{.cmdf = "ret", .byte = {0xC3,0,0},            .arg0_t = ARG_NONE,.arg1_t = ARG_NONE},
-};
-
-void asm_setsymbol(char* asmcode,SYMBOL_T symbols[],int8_t lastsymbol,uint32_t* pc) {
-	char temp[20] = {0};
-	char temp0[20] = {0};
-	uint8_t ptr = 0;
+void asm_setsymbol(char* asmcode,SYM_T* sym,uint32_t* pc) {
 	trim_c(asmcode,';');
 	trim_end(asmcode);
 	if (asmcode[0] == '\0') return; // For comments NEEDED REWORK
-	for (uint8_t i = 0; i < sizeof(x86) / sizeof(x86[0]); i++) {
-		if (stringscanf(asmcode,x86[i].cmdf,temp,temp0)) {
+	COMP_T s = {.asmcode = asmcode,.bytes = 0,.sym = sym,.pc = pc,.mode = 1};
 
-			for (uint8_t j = 0;x86[i].byte[j] != 0;j++) ptr++;
-
-			if (x86[i].arg0_t == ARG_INT8) {
-				ptr++;
+	GENED_T res = genbytecode(&s);
+	*pc += res.gened;
+	if (res.error) {
+		uint8_t c = 0; // For labels
+		arg_len(asmcode,&c,':');
+		if (asmcode[c+1] == ':') {
+			asmcode[++c] = '\0';
+			for (uint8_t i = 0;i < sym->lastsymbol;i++) {
+				if (strcmp(sym->symbols[i].text,asmcode) == 0) {
+					sym->symbols[i].address = *pc;
+					break;
+		        	}
 			}
-			else if (x86[i].arg0_t == ARG_INT32) {
-				ptr++;
-				ptr++;
-				ptr++;
-				ptr++;
-			} else if (x86[i].arg0_t == ARG_TAG) {
-				ptr++;
-			} else if (x86[i].arg0_t == ARG_REGB || x86[i].arg0_t == ARG_REGD) {
-				ptr++;
-				if (x86[i].arg1_t == ARG_INT8) ptr++;
-			}
-
-			*pc += ptr;
-		} else {
-			uint8_t c = 0; // For labels
-			arg_len(asmcode,&c,':');
-			if (asmcode[c+1] == ':') {
-				asmcode[++c] = '\0';
-				for (uint8_t i = 0;i < lastsymbol;i++) {
-					if (strcmp(symbols[i].text,asmcode) == 0) {
-						symbols[i].address = *pc;
-						break;
-			        	}
-				}
-		        }
-	        }
+        	}
 	}
 }
 
-int8_t asm_tobyte(char* asmcode, uint8_t* bytes, SYMBOL_T symbols[], int8_t lastsymbol,uint32_t* pc) {
-	char temp[20] = {0};
-	char temp0[20] = {0};
-	uint8_t ptr = 0;
+GENED_T asm_tobyte(char* asmcode, uint8_t* bytes, SYM_T* sym,uint32_t* pc) {
+	COMP_T s = {.asmcode = asmcode,.bytes = bytes,.sym = sym,.pc = pc,.mode = 0};
+
 	bool found = false;
 	trim_c(asmcode,';');
 	trim_end(asmcode);
-	if (asmcode[0] == '\0') goto nfound;
-	for (uint8_t i = 0; i < sizeof(x86) / sizeof(x86[0]); i++) {
-		if (x86[i].arg0_t == ARG_REGB) {
-			temp0[0] = 0xB0;
-			temp[0] =  0xB0;
-		}  else if (x86[i].arg0_t == ARG_REGD) {
-			temp0[0] = 0xD0;
-			temp[0] =  0xD0;
-		}
+	if (asmcode[0] == '\0') {
+		goto nfound;
+	}
 
-		if (stringscanf(asmcode,x86[i].cmdf,temp,temp0)) {
-			for (uint8_t j = 0; x86[i].byte[j] != 0 ;j++) {
-				bytes[ptr++] = x86[i].byte[j];
-			}
-
-			if (x86[i].arg0_t == ARG_INT8) {
-				bytes[ptr++] = (uint8_t)(temp[0] & 0xFF);
-			}
-			else if (x86[i].arg0_t == ARG_INT32) {
-				bytes[ptr++] = (uint8_t)(temp[0] & 0xFF);
-				bytes[ptr++] = (uint8_t)((temp[1] >> 8) & 0xFF);
-				bytes[ptr++] = (uint8_t)((temp[2] >> 16) & 0xFF);
-				bytes[ptr++] = (uint8_t)((temp[3] >> 24) & 0xFF);
-			} else if (x86[i].arg0_t == ARG_TAG) {
-				for (;lastsymbol >= 0;lastsymbol--) {
-					if (strcmp(symbols[lastsymbol].text,temp) == 0) {
-						bytes[ptr] = symbols[lastsymbol].address - *pc;
-						break;
-					}
-				}
-				ptr++;
-			} else if (x86[i].arg0_t == ARG_REGB || x86[i].arg0_t == ARG_REGD) {
-				bytes[ptr++] = temp[0];
-				if (x86[i].arg1_t == ARG_INT8) bytes[ptr++] = temp0[0];
-			}
-			*pc += ptr;
-			found = true;
-		}
+	GENED_T res = genbytecode(&s);
+	if (!res.error) {
+		found = true;
 	}
 
 nfound:
@@ -328,28 +139,33 @@ nfound:
 		uint8_t i = 0;
 		skip_chars(asmcode,&i);
 		arg_len(asmcode,&i,':');
-		if (asmcode[i+1] != ':' && asmcode[0] != '\0') ptr = -1;
+		if (asmcode[i+1] == ':' || asmcode[0] == '\0') {
+			res.error = 2;
+		} else {
+			res.error = 1;
+		}
 		// Function on start trims ; and
 		// if ; is first, asmcode[0] == '\0'
 	}
-	return ptr;
+	return res;
 }
 
-void asm_psymbol(char* buffer,SYMBOL_T symbols[],uint8_t* symbol) {
+void asm_psymbol(char* buffer,SYM_T* SYM) {
 	uint8_t i = 0;
 	skip_chars(buffer,&i);
 	char* ptr = &buffer[i];
 	arg_len(buffer,&i,':');
 	if (buffer[i+1] == ':') {
 		buffer[i+1] = '\0';
-		uint8_t idx = *symbol;
-		strcpy(symbols[idx].text,ptr);
-		*symbol = idx + 1;
+		uint8_t idx = SYM->lastsymbol;
+		strcpy(SYM->symbols[idx].text,ptr);
+		SYM->lastsymbol = idx + 1;
 	}
 }
 
 typedef struct {
 	bool showbytes;
+	bool debug;
 } SETTINGS_CMP;
 
 void asm_compile(FILE* file,FILE* bin,SETTINGS_CMP* settings) {
@@ -358,20 +174,20 @@ void asm_compile(FILE* file,FILE* bin,SETTINGS_CMP* settings) {
 	// bufferptr is pointer on char starting without \t ' '
 
 	SYMBOL_T symbols[50] = {0}; // For "label:"
-	int8_t lastsymbol = 0;
+	SYM_T sym = {.symbols = symbols,.lastsymbol = 0};
 
 	uint32_t pc = 0;
 
 	// Symbol parser
 	while (fgets(buffer,sizeof(buffer),file) != NULL) {
-		asm_psymbol(bufferptr,symbols,&lastsymbol);
+		asm_psymbol(bufferptr,&sym);
 	}
 
 	clearerr(file);
 	fseek(file,0,SEEK_SET);
 
 	while (fgets(buffer,sizeof(buffer),file) != NULL) {
-		asm_setsymbol(buffer,symbols,lastsymbol,&pc);
+		asm_setsymbol(buffer,&sym,&pc);
 	}
 
 	clearerr(file);
@@ -386,20 +202,17 @@ void asm_compile(FILE* file,FILE* bin,SETTINGS_CMP* settings) {
 		skip__chars(&bufferptr);
 		uint8_t byte[20] = {0};
 		lineasm++;
-		int8_t b_c = asm_tobyte(bufferptr,byte,symbols,lastsymbol,&pc);
-		/*
-			-1 : Not finded asm cmd
-			0  : Label/Tag ex. output:
-			>0 : Bytes of code
-		*/
-		if (b_c == -1) {
+
+		GENED_T b_c = asm_tobyte(bufferptr,byte,&sym,&pc);
+		if (b_c.error == 1) {
 			error = true;
 			break;
-		} else if (b_c > 0) {
-			fwrite(byte,b_c,1,bin);
+		} else if (b_c.error != 2) { // 2 if empty
+			pc += b_c.gened;
+			fwrite(byte,b_c.gened,1,bin);
 			if (settings->showbytes) {
 				printf("Line: %u | ",lineasm);
-				for (uint8_t i = 0; i < b_c;i++) {
+				for (uint8_t i = 0; i < b_c.gened;i++) {
 					printf("%X ",byte[i]);
 				}
 				printf("| %s |\n\r",bufferptr);
@@ -414,16 +227,28 @@ void asm_compile(FILE* file,FILE* bin,SETTINGS_CMP* settings) {
 	}
 }
 
+// -1 is ok
+int8_t arg_handler(uint8_t argc,char* argv[],SETTINGS_CMP* settings) { // argc is the first argument, not executefilename!
+	for (uint8_t i = 0;i <= argc;i++) {
+		if (argv[i] == NULL) break;
+		char* text = argv[i];
+		if (strcmp(text,"-bytes") == 0) {
+			settings->showbytes = true;
+		} else if (strcmp(text,"-debug") == 0) {
+			settings->debug = true;
+		} else {
+			return i;
+		}
+	}
+	return -1;
+}
+
 int main(int argc,char* argv[]) {
 	if (argc < 3) return 1;
 
 	// Compile settings
-	SETTINGS_CMP settings = {.showbytes = false};
-	if (argc >= 4) { // atasm test bin -bytes
-		if (strcmp(argv[3],"-bytes") == 0) {
-			settings.showbytes = true;
-		}
-	}
+	SETTINGS_CMP settings = {.showbytes = false,.debug = false};
+	arg_handler(argc-3,&argv[3],&settings); // atasm test bin -bytes
 
 	FILE* file = fopen(argv[1],"r");
 	FILE* bin =  fopen(argv[2],"w");
